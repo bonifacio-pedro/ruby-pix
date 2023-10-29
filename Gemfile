d660a84d5e3a3e2b61211714c16faa079657c1a7
bonifacio-pedro <pedrorosa.rb@gmail.com>
2023-10-28 21:37:00 -0300

    Refactor and robucop added

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'cpf_utils', '~> 1.3', '>= 1.3.1'
+gem 'rubocop'
 gem 'sqlite3'
 gem 'tty-spinner'
 gem 'tty-table'
--- app.rb
+++ app.rb
@@ -1,52 +1,55 @@
-require_relative 'pix'
-require_relative 'transaction'
 require_relative 'config'
+require_relative 'lib/pix'
+require_relative 'lib/transaction'
 require 'tty-spinner'
 require 'tty-table'
 
 system('clear')
-spinner = TTY::Spinner.new("[:spinner] Carregando sistema...", format: :pulse_2)
+spinner = TTY::Spinner.new('[:spinner] Carregando sistema...', format: :pulse_2)
 spinner.auto_spin
 sleep(2)
-spinner.stop("Pronto!")
+spinner.stop('Pronto!')
 puts "\n"
 
-if ARGV[0] == "--novo" or ARGV[0] == "-n"
-    if ARGV[1] != nil and PIX_TYPES.include?(ARGV[1])
-        if ARGV[2] != nil
-            sleep(1)
-            puts Pix.insert_data_new_pix(ARGV[1], ARGV[2])
-        else
-            puts "Adicione uma chave válida"
-        end
+case ARGV[0]
+when '--novo'
+  if !ARGV[1].nil? && Config::PIX_TYPES.include?(ARGV[1])
+    if !ARGV[2].nil?
+      sleep(1)
+      puts Pix.insert_data_new_pix(ARGV[1], ARGV[2])
     else
-        puts "Coloque um tipo de chave pix válido!
-        TIPOS VÁLIDOS:
-            - CPF
-            - TEL
-            - EMAIL"
+      puts 'Adicione uma chave válida'
     end
-elsif ARGV[0] == "--transacao" or ARGV[0] == "-t"
-    if ARGV[1] != nil and ARGV[2] != nil and ARGV[3] != nil and not ARGV[3].include?(',')
-        sleep(1)
-        transaction = Transaction.new(ARGV[1],ARGV[2],ARGV[3].to_f)
-    else
-        puts "Adicione chaves e um preço válido para a transação! exemplo:\nruby app.rb -t [CHAVE VÁLIDA] [CHAVE VÁLIDA] 10.99 (EXEMPLO DE PREÇO)"
-    end
-elsif ARGV[0] == "--transacoes" or ARGV[0] == "-ta"
-    table = TTY::Table.new(
-        header: ['Pagador','Recebedor','Data e hora','Valor'])
-    transactions = Transaction.return_all_transactions
-    transactions.each do |transaction|
-        transaction[2] = transaction[2].inspect.to_s[1..16]
-        table << transaction
-    end
-    puts table.render(:unicode)
-elsif ARGV[0] == "--help" or ARGV[0] == "-h"
-    puts HELPER
-elsif ARGV[0] == "--flush"
-    DB.execute "DELETE FROM Transactions"
-    DB.execute "DELETE FROM Pix"
+  else
+    puts 'Coloque um tipo de chave pix válido!
+    TIPOS VÁLIDOS:
+        - CPF
+        - TEL
+        - EMAIL'
+  end
+when '--transacao'
+  if !ARGV[1].nil? && !ARGV[2].nil? && !ARGV[3].nil? && !ARGV[3].include?(',')
+    sleep(1)
+    Transaction.new(ARGV[1], ARGV[2], ARGV[3].to_f)
+  else
+    puts "Adicione chaves e um preço válido para a transação! exemplo:\n
+    ruby app.rb -t [CHAVE VÁLIDA] [CHAVE VÁLIDA] 10.99"
+  end
+when '--transacoes'
+  table = TTY::Table.new(
+    header: ['Pagador', 'Recebedor', 'Data e hora', 'Valor']
+  )
+  transactions = Transaction.return_all_transactions
+  transactions.each do |t|
+    t[2] = t[2].inspect.to_s[1..16]
+    table << t
+  end
+  puts table.render(:unicode)
+when '--help' || '-h'
+  puts Config::HELPER
+when '--flush'
+  DB.execute 'DELETE FROM Transactions'
+  DB.execute 'DELETE FROM Pix'
 else
-    puts HELPER
+  puts Config::HELPER
 end
