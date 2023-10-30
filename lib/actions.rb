143aa9469a8cb536d103e6ab10d064dc79f8d79a
bonifacio-pedro <pedrorosa.rb@gmail.com>
2023-10-30 17:00:35 -0300

    Rubocop ajusts

--- lib/actions.rb
+++ lib/actions.rb
@@ -24,57 +24,57 @@ module Actions
           - CELL
           - EMAIL'
     end
-	end
+  end
 
-	# Search key
+  # Search key
   def self.search(args)
-	  return puts Pix.search_key_initial(args[:key]) if verify_args?(args)
+    return puts Pix.search_key_initial(args[:key]) if verify_args?(args)
 
-		puts 'Enter a valid search parameter' 
-	end
+    puts 'Enter a valid search parameter' 
+  end
 
-	# Insert new transaction
-	def self.new_transaction(args)
-		if self.verify_args?(args)
-			sleep(0.5)
-    	Transaction.new(args[:payer], args[:receiver], args[:price])
-		else
-			puts "Add keys and a valid price for the transaction! example:\n
+  # Insert new transaction
+  def self.new_transaction(args)
+    if self.verify_args?(args)
+      sleep(0.5)
+      Transaction.new(args[:payer], args[:receiver], args[:price])
+    else
+      puts "Add keys and a valid price for the transaction! example:\n
       ruby app.rb -t [VALID KEY] [VALID KEY] 10.99"
     end
-	end
+  end
 
-	# Show all transactions
-	def self.transactions
+  # Show all transactions
+  def self.transactions
     transactions = Query.return_all_transactions
     puts generate_table(%w[Id Payer Receiver Price Datetime], transactions)
-	end
+  end
 
-	# Generates a table
-	def self.generate_table(headers, items)
-		table = TTY::Table.new(
+  # Generates a table
+  def self.generate_table(headers, items)
+    table = TTY::Table.new(
       header: headers
     )
     items.map { |i| table << i }
     table.render(:unicode)
-	end
+  end
 
-	# Start app
-	def self.start_system
-		system('clear')
-		spinner = TTY::Spinner.new('[:spinner] Loading system...', format: :pulse)
-		spinner.auto_spin
-		sleep(2)
-		spinner.stop('Ready!')
-		sleep(1)
-		system('clear')
-	end
+  # Start app
+  def self.start_system
+    system('clear')
+    spinner = TTY::Spinner.new('[:spinner] Loading system...', format: :pulse)
+    spinner.auto_spin
+    sleep(2)
+    spinner.stop('Ready!')
+    sleep(1)
+    system('clear')
+  end
 
-	# Verify system args
-	def self.verify_args?(args)
+  # Verify system args
+  def self.verify_args?(args)
     args.each_value do |arg|
-		  return false if arg.nil?
-		end
-		true
-	end
+      return false if arg.nil?
+    end
+    true
+  end
 end
